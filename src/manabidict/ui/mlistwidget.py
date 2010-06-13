e298d5ac49e96bb09e09f1012a8d085c034c9280
Alex Ehlke <jehlke@student.umass.edu>
2010-06-12 23:05:50 -0400

    working on faster rendering

--- src/manabidict/ui/mlistwidget.py
+++ src/manabidict/ui/mlistwidget.py
@@ -5,9 +5,14 @@ from PyQt4.Qt import Qt
 class MListWidget(QListWidget):
     gainedFocus = pyqtSignal()
     lostFocus = pyqtSignal()
+
     def __init__(self, parent = None):
         QListWidget.__init__(self, parent)
-    
+
+        self._htmlItemLabel = None
+        self._htmlItemWidget = None
+
+        self._prepHtmlItemWidget()
 
     def focusInEvent(self, event):
         self.gainedFocus.emit()
@@ -17,50 +22,58 @@ class MListWidget(QListWidget):
         self.lostFocus.emit()
         event.accept()
 
+    def _prepHtmlItemWidget(self):
+        self._htmlItemWidget = QTextEdit(parent=self)
+        hw = self._htmlItemWidget
+        #hw.setText('test')
+
+        hw.setFont(self.font())
+        hw.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        hw.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        f = QTextFrameFormat()
+        f.setMargin(0)
+        f.setPadding(0)
+        f.setBorder(0)
+        hw.document().rootFrame().setFrameFormat(f)
+        hw.viewport().setAutoFillBackground(False)
+
+        #label.setText('<b>hello</b>')
+        #label.setText('yeah<img src=":/gaiji/confused.png"><img src=":/gaiji/confused.png"><img src=":/gaiji/confused.png">'+html) #setHtml to force html
+        hw.setReadOnly(True)
+        hw.setFrameShape(QFrame.NoFrame)
+        hw.setTextInteractionFlags(Qt.NoTextInteraction)
+
+        font_metrics = QFontMetrics(hw.currentFont())
+        h = font_metrics.height() + hw.frameWidth() * 2 + 1
+        #hw.setFixedHeight(h)
+        hw.setFixedHeight(18)
+        hw.setFixedWidth(self.size().width() - 4)
+
+
+
     def addHtmlItem(self, html, data):
         '''Adds a new QTextEdit widget containing the given HTMl as an item in the list.
         '''
         item = QListWidgetItem(u'')
         item.setData(Qt.UserRole, data)
         self.addItem(item)
-        label = QTextEdit(parent=self)
-        #print self.font().pixelSize()
-        label.setFont(self.font())
-        #label.setMaximumSize(QSize(100, 20))
-        label.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        label.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        #label.setPlainText('what huhhhh what')
-        label.setText(html)
+        self._htmlItemWidget.setHtml(html)
 
-        f = QTextFrameFormat()
-        f.setMargin(0)
-        f.setPadding(0)
-        f.setBorder(0)
-        label.document().rootFrame().setFrameFormat(f)
-        label.viewport().setAutoFillBackground(False)
+        pm = QImage(self._htmlItemWidget.size(), QImage.Format_ARGB32)
+        #pm = QPixmap(self._htmlItemWidget.size())
+        pm.fill(Qt.transparent)
+        self._htmlItemWidget.viewport().render(pm, flags=QWidget.DrawChildren)
 
-        #label.setText('<b>hello</b>')
-        #label.setText('yeah<img src=":/gaiji/confused.png"><img src=":/gaiji/confused.png"><img src=":/gaiji/confused.png">'+html) #setHtml to force html
-        label.setReadOnly(True)
-        label.setFrameShape(QFrame.NoFrame)
+        item.setSizeHint(self._htmlItemWidget.frameSize())
+
+        label = QLabel()
         label.setTextInteractionFlags(Qt.NoTextInteraction)
 
-        font_metrics = QFontMetrics(label.currentFont())
-        h = font_metrics.height() + label.frameWidth() * 2 + 1
-        label.setFixedHeight(h)
-        label.setFixedWidth(self.size().width() - 4)
-
-        item.setSizeHint(label.frameSize())
+        label.setPixmap(QPixmap.fromImage(pm))
         
-        pm = QImage(label.size(), QImage.Format_ARGB32)
-        pm.fill(Qt.transparent)
-        #label.viewport().render(pm, flags=QWidget.DrawChildren)
-        label.render(pm, flags=QWidget.DrawChildren)
-
-        label2 = QLabel()
-        label2.setPixmap(QPixmap.fromImage(pm))
-        self.setItemWidget(item, label2)
+        self.setItemWidget(item, label)
 
         #self.setItemWidget(item, label)
         #self.
